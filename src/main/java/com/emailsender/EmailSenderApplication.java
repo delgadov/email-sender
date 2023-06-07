ca2c971d6a1ad194b244c2e5559299ecca9a68a0
Vinicius Martins Delgado <viniciusmardel@gmail.com>
2023-06-07 15:32:06 -0300

    Removed @ComponentScan

--- src/main/java/com/emailsender/EmailSenderApplication.java
+++ src/main/java/com/emailsender/EmailSenderApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.emailsender")
 public class EmailSenderApplication {
 
 	public static void main(String[] args) {
